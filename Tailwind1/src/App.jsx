c4a37f318250469feca135e8f4b7dd1eb98097d5
Bharat <kadamb208@gmail.com>
2024-09-29 17:52:53 +0530

    adding container styles

--- Tailwind1/src/App.jsx
+++ Tailwind1/src/App.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-pink-600'> App</div>
+   
+    <div>
+      <h1 className='xs:text-center sm:text-left md:text-right lg:text-center bg-blue-500 text-ws text-red-700'>first div </h1>
+      <div>
+        <h1 className='max-w-[1600px] mx-auto bg-[#4b5563] '>second div</h1>
+      </div>
+    </div>
+   
   )
 }
 
