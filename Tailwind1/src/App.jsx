4422886a8cf164637a8910fa8319ac4aa12127d3
Bharat <kadamb208@gmail.com>
2024-09-30 18:18:30 +0530

    seconde commit -m

--- Tailwind1/src/App.jsx
+++ Tailwind1/src/App.jsx
@@ -7,10 +7,12 @@ const App = () => {
       <h1 className='xs:text-center sm:text-left md:text-right lg:text-center bg-blue-500 text-ws text-red-700'>first div </h1>
       <div>
         <h1 className='max-w-[1600px] mx-auto bg-[#4b5563] '>second div</h1>
+
+        <h2> hello world</h2>
       </div>
     </div>
    
   )
 }
 
-export default App
+export default App   
