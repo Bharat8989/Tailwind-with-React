d84759c46a19a74261a752cca1f55d0102bd5153
Bharat <kadamb208@gmail.com>
2024-10-01 18:12:50 +0530

    adding files Grid template rows

---
+++ Tailwind1/src/Grid.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+
+const Grid = () => {
+  return (
+    <>
+      <div className="max-w-[1170px] mx-auto">
+        <h1 className="text-[20px] md:text-[30px] lg:text-[40px] font-bold text-center  py-[40px]">
+          Our Services
+        </h1>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-[20px]">
+          <div className="shadow-lg pb-[20px]">
+            <img
+              className="w-[100%]"
+              src="https://wallpapercave.com/wp/wp4088642.jpg"
+              alt="img"
+            />
+            <div className="ps-[20px]">
+              <h4 className="text-[20px] font-bold font-mono">
+                Live Online Courses
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis consequatur corporis neque vero assumenda officiis
+                voluptatem quam cupiditate provident, nostrum harum nesciunt
+                nobis?
+              </p>
+            </div>
+          </div>
+
+          <div className="shadow-lg pb-[20px]">
+            <img
+              className="w-[100%]"
+              src="https://wallpapercave.com/wp/wp4088642.jpg"
+              alt="img"
+            />
+            <div className="ps-[20px]">
+              <h4 className="text-[20px] font-bold font-mono">
+                Live Online Courses
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis consequatur corporis neque vero assumenda officiis
+                voluptatem quam cupiditate provident, nostrum harum nesciunt
+                nobis?
+              </p>
+            </div>
+          </div>
+          <div className="shadow-lg pb-[20px]">
+            <img
+              className="w-[100%]"
+              src="https://wallpapercave.com/wp/wp4088642.jpg"
+              alt="img"
+            />
+            <div className="ps-[20px]">
+              <h4 className="text-[20px] font-bold font-mono">
+                Live Online Courses
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis consequatur corporis neque vero assumenda officiis
+                voluptatem quam cupiditate provident, nostrum harum nesciunt
+                nobis?
+              </p>
+            </div>
+          </div>
+          <div className="shadow-lg pb-[20px]">
+            <img
+              className="w-[100%]"
+              src="https://wallpapercave.com/wp/wp4088642.jpg"
+              alt="img"
+            />
+            <div className="ps-[20px]">
+              <h4 className="text-[20px] font-bold font-mono">
+                Live Online Courses
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis consequatur corporis neque vero assumenda officiis
+                voluptatem quam cupiditate provident, nostrum harum nesciunt
+                nobis?
+              </p>
+            </div>
+          </div>
+          <div className="shadow-lg pb-[20px]">
+            <img
+              className="w-[100%]"
+              src="https://wallpapercave.com/wp/wp4088642.jpg"
+              alt="img"
+            />
+            <div className="ps-[20px]">
+              <h4 className="text-[20px] font-bold font-mono">
+                Live Online Courses
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis consequatur corporis neque vero assumenda officiis
+                voluptatem quam cupiditate provident, nostrum harum nesciunt
+                nobis?
+              </p>
+            </div>
+          </div>
+
+          <div className="shadow-lg pb-[20px]">
+            <img
+              className="w-[100%]"
+              src="https://wallpapercave.com/wp/wp4088642.jpg"
+              alt="img"
+            />
+            <div className="ps-[20px]">
+              <h4 className="text-[20px] font-bold font-mono">
+                Live Online Courses
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis consequatur corporis neque vero assumenda officiis
+                voluptatem quam cupiditate provident, nostrum harum nesciunt
+                nobis?
+              </p>
+            </div>
+          </div>
+          <div className="shadow-lg pb-[20px]">
+            <img
+              className="w-[100%]"
+              src="https://wallpapercave.com/wp/wp4088642.jpg"
+              alt="img"
+            />
+            <div className="ps-[20px]">
+              <h4 className="text-[20px] font-bold font-mono">
+                Live Online Courses
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis consequatur corporis neque vero assumenda officiis
+                voluptatem quam cupiditate provident, nostrum harum nesciunt
+                nobis?
+              </p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </>
+  );
+};
+
+export default Grid;
