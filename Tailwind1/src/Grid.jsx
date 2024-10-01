515d11a98d3fe478400414374380e29a74ec9568
Bharat <kadamb208@gmail.com>
2024-10-01 18:35:04 +0530

    leading gab and auto

--- Tailwind1/src/Grid.jsx
+++ Tailwind1/src/Grid.jsx
@@ -138,8 +138,45 @@ const Grid = () => {
             </div>
           </div>
         </div>
-        
       </div>
+      <section className="bg-slate-300 py-10">
+        <h1 className="text-[20px] md:text-[30px] lg:text-[40px] font-bold text-center  py-[40px]">
+          About Us
+        </h1>
+        <div className="max-w-[1170px] mx-auto  grid grid-cols-1 md:grid-cols-[40%_auto] gap-[30px]">
+          <div>
+          <img
+              className="w-[100%]"
+              src="https://wallpapercave.com/wp/wp4088642.jpg"
+              alt="img"
+            />
+          </div>
+          <div>
+            <h3 className="text-[30px] pb-[30px] font-bold">Welcome to GCOEC</h3>
+            <p className="leading-5 mb-[10px] text-[18px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis consequatur corporis neque vero assumenda officiis
+              voluptatem quam cupiditate provident, nostrum harum nesciunt
+              nobis?
+            </p>
+            <p className="leading-6 mb-[10px] text-[18px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis consequatur corporis neque vero assumenda officiis
+              voluptatem quam cupiditate provident, nostrum harum nesciunt
+              nobis?
+            </p>
+            <p className="leading-6 mb-[10px] text-[18px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis consequatur corporis neque vero assumenda officiis
+              voluptatem quam cupiditate provident, nostrum harum nesciunt
+              nobis?
+            </p>
+            
+            
+
+          </div>
+        </div>
+      </section>
     </>
   );
 };
